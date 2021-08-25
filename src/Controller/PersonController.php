221270f3b4b535c599f11ea423b8c50228313719
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-25 16:14:38 +0200

    mj

---
+++ src/Controller/PersonController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class PersonController extends AbstractController
+{
+    /**
+     * @Route("/person", name="person")
+     */
+    public function index1(): Response
+    {
+        return $this->render('person/index.html.twig', [
+            'controller_name' => 'PersonController',
+        ]);
+    }
+    /**
+    * @Route("/api/person", name="api_person", methods="GET")
+    */
+    public function index2(PersonRepository $personRepository,NormalizerInterface $normalizer): Response
+    {
+    $personnes = $personRepository->findAll();
+    $normalized = $normalizer->normalize($personnes);
+    $json = json_encode($normalized);
+    $reponse = new Response($json, 200, [
+    'content-type' => 'application/json'
+    ]);
+    return $reponse;
+    }
+}
