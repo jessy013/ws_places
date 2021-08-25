72bdf47c6d851d024559fdb8e10d6c4e4d13d103
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-25 11:49:12 +0200

    mj

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -6,13 +6,14 @@ use App\Repository\PlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class PlaceController extends AbstractController
 {
     /*
     * @Route("/api/place", name="api_place")
     */
- public function index(PlaceRepository $placeRepository): Response
+ public function index1(PlaceRepository $placeRepository): Response
  {
  $places = $placeRepository->findAll();
   $json = json_encode($places);
@@ -21,19 +22,32 @@ class PlaceController extends AbstractController
  ]);
  return $reponse;
 }
-/**
-* @Route("/api/place/{id}", name="api_place_avec_id")
-*/
-public function findById(PlaceRepository $placeRepository): Response
-{
-$place = $placeRepository->find($id);
-$json = json_encode($place);
-$reponse = new Response($json, 200, [
-'content-type' => 'application/json'
-]);
-return $reponse;
+ /**
+ * @Route("/api/place/{id}", name="api_place_avec_id")
+ */
+ public function findById(PlaceRepository $placeRepository,$id): Response
+ {
+    $place = $placeRepository->find($id);
+    $json = json_encode($place);
+    $reponse = new Response($json, 200, [
+    'content-type' => 'application/json'
+    ]);
+    return $reponse;
 }
-        
+        /**
+    * @Route("/api/place", name="api_place" , methods="GET")
+    */
+    public function index2(PlaceRepository $placeRepository
+    ,NormalizerInterface $normalizer): Response
+    {
+    $places = $placeRepository->findAll();
+    $normalized = $normalizer->normalize($places);
+    $json = json_encode($normalized);
+    $reponse = new Response($json, 200, [
+    'content-type' => 'application/json'
+    ]);
+    return $reponse;
+}  
     
     
 }
