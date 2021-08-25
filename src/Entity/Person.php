0799199238852fa8342db0f2746c692a82478656
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-25 15:02:55 +0200

    lol

---
+++ src/Entity/Person.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonRepository::class)
+ */
+class Person
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=place::class, inversedBy="likedBy")
+     */
+    private $placesLiked;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $FirstName;
+
+    public function __construct()
+    {
+        $this->placesLiked = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|place[]
+     */
+    public function getPlacesLiked(): Collection
+    {
+        return $this->placesLiked;
+    }
+
+    public function addPlacesLiked(place $placesLiked): self
+    {
+        if (!$this->placesLiked->contains($placesLiked)) {
+            $this->placesLiked[] = $placesLiked;
+        }
+
+        return $this;
+    }
+
+    public function removePlacesLiked(place $placesLiked): self
+    {
+        $this->placesLiked->removeElement($placesLiked);
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->FirstName;
+    }
+
+    public function setFirstName(string $FirstName): self
+    {
+        $this->FirstName = $FirstName;
+
+        return $this;
+    }
+}
--- src/Entity/Place.php
+++ src/Entity/Place.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PlaceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Place
      */
     private $name;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Person::class, mappedBy="placesLiked")
+     */
+    private $likedBy;
+
+    public function __construct()
+    {
+        $this->likedBy = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,33 @@ class Place
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Person[]
+     */
+    public function getLikedBy(): Collection
+    {
+        return $this->likedBy;
+    }
+
+    public function addLikedBy(Person $likedBy): self
+    {
+        if (!$this->likedBy->contains($likedBy)) {
+            $this->likedBy[] = $likedBy;
+            $likedBy->addPlacesLiked($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLikedBy(Person $likedBy): self
+    {
+        if ($this->likedBy->removeElement($likedBy)) {
+            $likedBy->removePlacesLiked($this);
+        }
 
         return $this;
     }
