b8c44bc2e153562b8a77d85165761f32b5a8033d
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-30 11:18:56 +0200

    maj

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Place;
 use App\Repository\PlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class PlaceController extends AbstractController
 {
@@ -37,18 +41,28 @@ class PlaceController extends AbstractController
     return $reponse;
 }
         /**
-    * @Route("/api/place", name="api_place" , methods="GET")
+    * @Route("/api/place", name="api_place_add" , methods="POST")
     */
-    public function index2(PlaceRepository $placeRepository
-    ,NormalizerInterface $normalizer): Response
+    public function index2(EntityManagerInterface $entityManager, Request $request, SerializerInterface $serializer, ValidatorInterface $validator): Response
     {
-    $places = $placeRepository->findAll();
-    $normalized = $normalizer->normalize($places,null,['groups'=>'place:read']);
-    $json = json_encode($normalized);
-    $reponse = new Response($json, 200, [
-    'content-type' => 'application/json'
-    ]);
-    return $reponse;
+ $contenu = $request->getContent();
+        try
+    {
+        $place = $serializer->deserialize($contenu, Place::class, 'json');
+        $errors = $validator->validate($place);
+        if (count($errors) > 0)
+    {
+        return $this->json($errors, 400);
+    }
+        $entityManager->persist($place);
+        $entityManager->flush();
+        return $this->json($place, 201, [],
+        ['groups' => 'place:read']);
+    }
+        catch (NotEncodableValueException $e)
+    {
+        return $this->json(['status' => 400,'message' => $e->getMessage()]);
+    }
 }  
   
 }
