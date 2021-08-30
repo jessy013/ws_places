67554890f6133f31014a1a170173aff3831e62f9
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-30 14:27:19 +0200

    Update AuthController.php

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,15 +2,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerIdentifier;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderIdentifier;
+use Symfony\Component\Security\Core\User\UserIdentifier;
 class AuthController extends ApiController
 {
-public function register(Request $request, UserPasswordEncoderInterface $encoder)
+public function register(Request $request, UserPasswordEncoderIdentifier $encoder)
 {
 $em = $this->getDoctrine()->getManager();
 $request = $this->transformJsonBody($request);
@@ -25,14 +25,14 @@ $user->setPassword($encoder->encodePassword($user, $password));
 $user->setUsername($username);
 $em->persist($user);
 $em->flush();
-return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
+return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUserIdentifier()));
 }
 /**
-* @param UserInterface $user
-* @param JWTTokenManagerInterface $JWTManager
+* @param UserIdentifier $user
+* @param JWTTokenManagerIdentifier $JWTManager
 * @return JsonResponse
 */
-public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
+public function getTokenUser(UserIdentifier $user, JWTTokenManagerIdentifier $JWTManager)
 
 {
 return new JsonResponse(['token' => $JWTManager->create($user)]);
